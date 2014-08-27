89891689a3e503d84e2c8ef59b38efcadf2d7a34
jcdesign <juan14nob@gmail.com>
2014-08-26 22:39:40 -0300

    changes

--- CropJC/upload.php
+++ CropJC/upload.php
@@ -1,24 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 include_once("cropjc.php");
-//print_r($_POST["dimensions"]);exit;
 
-/*
-print_r($_POST);
-print_r($_FILES);
-exit;
-*/
-
-//$image = file_get_contents("imagetest.jpg");
-
-/*
-	"xPos" => 0,
-	"yPos" => 0,
-	"cropH" => 200,
-	"cropW" => 200,
-*/
 $default = array(
 		
 	"destination_path" => "resources",
